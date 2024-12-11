7363a07d20fbf7afe9b2626b81c5dbd856df4e2b
nguyenthanhlam20 <nguyenthanhlam1070@gmail.com>
2024-12-11 22:56:01 +0700

    .

--- backend/BusinessObjects/Models/SeedCenterContext.cs
+++ backend/BusinessObjects/Models/SeedCenterContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 namespace BusinessObjects.Models
 {
@@ -33,8 +34,12 @@ namespace BusinessObjects.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server=LAPTOP-ALU23ESG;database=SeedCenter;uid=sa;pwd=55555;TrustServerCertificate=true");
+                var config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                optionsBuilder.UseSqlServer(config.GetConnectionString("DB"));
             }
         }
 
